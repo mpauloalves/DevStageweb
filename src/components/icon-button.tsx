b112a6c54704e8a0e65a06d1124ca81f2d645f08
mpauloalves <marcospauloas17@gmail.com>
2025-02-18 20:38:43 -0300

    first commit

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,11 @@
+import type { ComponentProps } from "react"
+
+interface IconButtonProps extends ComponentProps<'button'>{}
+export function IconButton(props: IconButtonProps) {
+    return (
+    <button
+     className="p-1.5 bg-gray-500 text-blue rounded-md  cursor-pointer transition-colors duration-200 hover:bg-blue hover:text-gray-900 "
+   {...props}
+     />
+)   
+}
