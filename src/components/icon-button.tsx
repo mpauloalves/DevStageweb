03d5a57cf5f8d3636af79b1e461b5edfc43c7bf0
mpauloalves <marcospauloas17@gmail.com>
2025-02-19 22:28:17 -0300

    feat: add invite page with React Hook Form and Zod validations

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,10 +1,11 @@
 import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
 
 interface IconButtonProps extends ComponentProps<'button'>{}
-export function IconButton(props: IconButtonProps) {
+export function IconButton({className, ...props}: IconButtonProps) {
     return (
     <button
-     className="p-1.5 bg-gray-500 text-blue rounded-md  cursor-pointer transition-colors duration-200 hover:bg-blue hover:text-gray-900 "
+     className={twMerge("p-1.5 bg-gray-500 text-blue rounded-md  cursor-pointer transition-colors duration-200 hover:bg-blue hover:text-gray-900", className)}
    {...props}
      />
 )   
